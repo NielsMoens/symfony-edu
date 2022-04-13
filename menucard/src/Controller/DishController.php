6906f2ec196e8a44996a16ede8879da0bf8d8779
NielsMoens <nielmoen@student.arteveldehs.be>
2022-04-13 17:59:13 +0200

    :fire: added a remove and create option for employees

--- menucard/src/Controller/DishController.php
+++ menucard/src/Controller/DishController.php
@@ -7,6 +7,7 @@ use App\Form\DishType;
 use App\Repository\DishRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,18 +26,41 @@ class DishController extends AbstractController
     }
 
     #[Route('/create', name: 'create')]
-    public function create(ManagerRegistry $doctrine): Response
+    public function create(ManagerRegistry $doctrine, Request $request): Response
     {
-        //form
         $dish = new Dish();
-        $form = $this->createForm(DishType::class, $dish);
 
-        $entityManager = $doctrine->getManager();
-//        $entityManager->persist($dish);
-//        $entityManager->flush();
+        //form
+        $form = $this->createForm(DishType::class, $dish);
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted()){
+            //Entity Manager
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($dish);
+            $entityManager->flush();
+
+            return $this->redirect($this->generateUrl('dish.edit'));
+        }
+
 
         return $this->render('dish/createForm.html.twig', [
             'createForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/remove{id}', name: 'remove')]
+    public function remove($id, DishRepository $dsh, ManagerRegistry $doctrine ): Response
+    {
+        $entityManager = $doctrine->getManager();
+        // find() returns an object instead of an array like with findAll()
+        $dish = $dsh->find($id);
+        $entityManager->remove($dish);
+        $entityManager->flush();
+
+        //message
+        $this->addFlash('succes', 'Dish was removed successfully');
+
+        return $this->redirect($this->generateUrl('dish.edit'));
     }
 }
